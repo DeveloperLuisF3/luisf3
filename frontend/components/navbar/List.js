173a2d041be47d308fb0ebfe36fe5590c3871609
Developer LuisF3 <developerluisfernando3@gmail.com>
2021-03-14 20:24:07 -0600

    [MOD, List.js][MOD, styles_sintaxis]

--- frontend/components/navbar/List.js
+++ frontend/components/navbar/List.js
@@ -1,4 +1,4 @@
-/*import { makeStyles } from "@material-ui/core/styles";*/
+import { makeStyles } from "@material-ui/core/styles";
 import Link from "next/link";
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
@@ -9,35 +9,46 @@ import InboxIcon from "@material-ui/icons/MoveToInbox";
 import MailIcon from "@material-ui/icons/Mail";
 import Home from "@material-ui/icons/Home";
 import CodeIcon from "@material-ui/icons/Code";
-/*
+
 const useStyles = makeStyles((theme) => ({
   a: {
-    color: theme.palette.primary.main,
+    color: theme.palette.text.secondary,
   },
-}));*/
+  aText: {
+    color: theme.palette.text.primary,
+  }
+}));
 
 export default function list() {
-  /*const classes = useStyles();*/
+  const classes = useStyles();
   return (
     <div>
       <List>
         <ListItem button>
           <ListItemIcon>
-            <Home  />
+            <Link href="/">
+              <a className={classes.a}>
+                <Home />
+              </a>
+            </Link>
           </ListItemIcon>
           <ListItemText>
             <Link href="/">
-              <a /*className={classes.a}*/>Home</a>
+              <a className={classes.aText}>Home</a>
             </Link>
           </ListItemText>
         </ListItem>
         <ListItem button>
           <ListItemIcon>
-            <CodeIcon  />
+            <Link href="/services">
+              <a className={classes.a}>
+                <CodeIcon />
+              </a>
+            </Link>
           </ListItemIcon>
           <ListItemText>
             <Link href="/services">
-              <a /*className={classes.a}*/>Services</a>
+              <a className={classes.aText}>Services</a>
             </Link>
           </ListItemText>
         </ListItem>
@@ -47,11 +58,7 @@ export default function list() {
         {["All", "mail", "Trash", "Spam"].map((text, index) => (
           <ListItem button key={text}>
             <ListItemIcon>
-              {index % 2 === 0 ? (
-                <InboxIcon />
-              ) : (
-                <MailIcon />
-              )}
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
             </ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
