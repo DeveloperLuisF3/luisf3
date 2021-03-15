2202b718106b04cf0c4be7c76d8be5d4e846695b
Developer LuisF3 <developerluisfernando3@gmail.com>
2021-03-14 20:26:03 -0600

    [MOD, firebase.config.js][ADD, loginGoogle]

--- frontend/firebase/firebase.config.js
+++ frontend/firebase/firebase.config.js
@@ -1,38 +1,81 @@
-import firebase from "firebase/app"
-import "firebase/database"
+import { ContactSupportOutlined, LeakRemove } from "@material-ui/icons";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT,
-}
+let firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT,
+};
 
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig)
-}
+  firebase.initializeApp(firebaseConfig)
+};
 
-export default firebase
-/*
-!firebase.apps.length && firebase.initializeApp(firebaseConfig)*/
+let db = firebase.firestore();
+let usersRef = db.collection('users');
 
-/*var dbContent
-var database = firebase.database()
-var themeRef = database.ref(`/theme/ThemeMode`)
+export let loginWithGoogle = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  return firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      var user = result.user;
+      console.log(user);
+      localStorage.setItem('user', JSON.stringify(result.user));
+      createUser(user);
+      return result.user;
+    }).catch((error) => {
+      console.log(error);
+    });
+};
 
-export function getThemeData() {
-  themeRef.on (
-    "value", (
-      snapshot => {
-        console.log(snapshot.val())
-        dbContent = snapshot.val()
-        console.log(dbContent)
-      }
-    )
-  )
-  return dbContent
-}*/
+let createUser = (user) => {
+  let name = user.displayName;
+  let email = user.email;
+  let image = user.photoURL;
+  let userId = user.uid;
+  let userObject = { name, email, image, userId };
+  console.log(userObject);
+  usersRef.doc(userObject.userId).set({ userObject })
+    .then(() => {
+      console.log("User Created");
+    })
+    .catch((error) => {
+      console.log("error");
+    });
+};
+
+export let logOut = () => {
+  firebase.auth().signOut();
+  localStorage.removeItem('user');
+};
+
+export let deleteUser = () => {
+  let delUser = firebase.auth().currentUser;
+  console.log(delUser.uid);
+  usersRef.doc(delUser.uid).delete()
+    .then(() => {
+      console.log("User Data Delete");
+      leave(delUser);
+    }).catch((error) => {
+      console.log("Error removing document: ", error);
+    });
+};
+
+let leave = (delUser) => {
+  delUser.delete().then(() => {
+    console.log("User Deleted");
+  }).catch((error) => {
+    console.log(error);
+  });
+};
+
+export default firebase;
