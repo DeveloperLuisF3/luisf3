766847523894f157b9f1087346b0fcff3b335cea
Developer LuisF3 <developerluisfernando3@gmail.com>
2021-03-14 20:28:56 -0600

    [MOD, index.js][DEL, content => ADD, userState]

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,55 +1,23 @@
-import React, { useEffect } from "react"
-import Head from 'next/head'
-import Link from 'next/link'
-import Layout, { siteTitle } from '../components/layout/layout'
-import clsx from "clsx"
-import { makeStyles } from "@material-ui/core/styles"
-import Drawer from "@material-ui/core/Drawer"
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles"
-import purple from "@material-ui/core/colors/purple"
-import green from "@material-ui/core/colors/green"
-import List from "../components/navbar/List"
-import AppBar from "../components/navbar/AppBar"
-import Box from "@material-ui/core/Box"
-import Container from "@material-ui/core/Container"
-import firebase from "../firebase/firebase.config"
+import React, { useEffect } from "react";
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout/layout";
 
-const useStyles = makeStyles({
-    list: {
-        width: 250,
-    },
-    fullList: {
-        width: "auto",
-    },
-})
+// Material Ui
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
 
-/*const ThemeModeConfig = (topic) => {
-    console.log(topic)
-    let ThemeMode = topic
-    themeRef.set(ThemeMode)
-}*/
-/*export async function getStaticProps() {
-    const topic = await getThemeData()
-    return {
-        props: {
-            topic,
-        },
-    }
-}*/
+// components
+import AppBar from "../components/navbar/AppBar";
 
-export default function HomePage({ topic }) {
-    const classes = useStyles()
-    console.log(topic)
+// context
+import UserState from "../context/Users/UserState";
+/*import { loginWithFacebook } from "../firebase/firebase.config";*/
 
-    const [ThemeMode, setThemeMode] = React.useState(topic || 'light')
+export default function HomePage() {
 
-    useEffect(() => {
-        let database = firebase.database()
-        let themeRef = database.ref(`/theme`)
-        themeRef.set(ThemeMode)
-        console.log(ThemeMode)
-    })
-    
+    const [ThemeMode, setThemeMode] = React.useState("light");
+
     const theme = createMuiTheme({
         palette: {
             type: ThemeMode,
@@ -63,91 +31,45 @@ export default function HomePage({ topic }) {
                 main: ThemeMode === "light" ? purple[500] : purple[200],
                 dark: ThemeMode === "light" ? purple[800] : purple[300],
             },
+            backgroundContainer: {
+                main: ThemeMode === "light" ? '#ffffff' : '#121212',
+            },
+            backgroundNavbar: {
+                main: ThemeMode === "light" ? green[500] : '#121212',
+            },
         },
-    })
-
-    const [state, setState] = React.useState({
-        top: false,
-        left: false,
-        bottom: false,
-        right: false,
     });
 
-    const toggleDrawer = (anchor, open) => (event) => {
-        if (
-            event.type === "keydown" &&
-            (event.key === "Tab" || event.key === "Shift")
-        ) {
-            return;
-        }
-        setState({ ...state, [anchor]: open });
-    }
+    /*const handleFacebook = () => {
+        loginWithFacebook();
+    }*/
 
-    const list = (anchor) => (
-        <div
-            className={clsx(classes.list, {
-                [classes.fullList]: anchor === "top" || anchor === "bottom",
-            })}
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
-        >
-            <List />
-        </div>
-    )
+   
     const handleDarkMode = () => {
-        setThemeMode("dark")
-    }
+        setThemeMode("dark");
+    };
     const handleLightMode = () => {
-        setThemeMode("light")
-    }
+        setThemeMode("light");
+    };
 
     return (
-        <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <UserState>
             <Layout>
                 <Head>
                     <title>{siteTitle}</title>
                 </Head>
-                {["left"].map((anchor) => (
-                    <React.Fragment key={anchor}>
-                        <AppBar
-                            toggleDrawer={toggleDrawer}
-                            ThemeMode={ThemeMode}
-                            DarkMode={handleDarkMode}
-                            LightMode={handleLightMode}
-                        />
-                        <Drawer
-                            anchor={anchor}
-                            open={state[anchor]}
-                            onClose={toggleDrawer(anchor, false)}
-                        >
-                            {list(anchor)}
-                        </Drawer>
-                    </React.Fragment>
-                ))}
-                <div>Welcome to Next.js!</div>
-                <h1>index</h1>
-                <h2 className="title">
-                Read{" "}
-                    <Link href="/services">
-                        <a>this page!</a>
-                    </Link>
-                </h2>
-                <Container>
-                    <Box my={2}>
-                        {[...new Array(12)]
-                        .map(
-                            () => `
-                                Cras mattis consectetur  purus        sit    amet fermentum.
-                                Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-                                Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-                                Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-                            `
-                        )
-                        .join("\n")}
-                    </Box>
-                </Container>
+                <AppBar
+                    /*handleFacebook={handleFacebook}*/
+                    ThemeMode={ThemeMode}
+                    DarkMode={handleDarkMode}
+                    LightMode={handleLightMode}
+                >
+
+                </AppBar>
             </Layout>
-        </ThemeProvider>
+        </UserState>
+      </ThemeProvider>
     );
 }
+
