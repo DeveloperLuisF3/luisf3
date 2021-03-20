656ec3667945ea989057794e656130fcd2a5bbff
Developer LuisF3 <developerluisfernando3@gmail.com>
2021-03-20 02:22:12 -0600

    [MOD, UserReducer.js][ADD, HANDLE_THEME]

--- context/Users/UserReducer.js
+++ context/Users/UserReducer.js
@@ -1,19 +1,24 @@
-import { HANDLE_LOGIN, USER_SESION } from "../types";
+import { HANDLE_LOGIN, USER_SESION, HANDLE_THEME } from "../types";
 
 let context = (state, action) => {
-      const { payload, type } = action;
-      switch (type) {
-            case HANDLE_LOGIN:
-                  return {
-                        ...state,
-                        login: payload
-                  };
-            case USER_SESION:
-                  return {
-                        ...state,
-                        login: payload
-                  };
-      }
+	const { payload, type } = action;
+	switch (type) {
+		case HANDLE_LOGIN:
+			return {
+				...state,
+				login: payload
+			};
+		case USER_SESION:
+			return {
+				...state,
+				login: payload
+			};
+		case HANDLE_THEME:
+			return {
+				...state,
+				ThemeMode: payload
+			};
+	}
 };
 
 export default context;
