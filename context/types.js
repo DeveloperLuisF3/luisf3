6562f744068cae14378e8f96733fd352cc16f6a5
Developer LuisF3 <developerluisfernando3@gmail.com>
2021-03-20 02:25:21 -0600

    [MOD, types.js][ADD, HANDLE_THEME]

--- context/types.js
+++ context/types.js
@@ -1,2 +1,3 @@
-export const HANDLE_LOGIN = "HANDLE_LOGIN";
-export const USER_SESION = "USER_SESION";
+export let HANDLE_LOGIN = "HANDLE_LOGIN";
+export let USER_SESION = "USER_SESION";
+export let HANDLE_THEME = "HANDLE_THEME";
